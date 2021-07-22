023b622a194838482599fc8b66a2b0cd1ca32447
andi0b <andix25@gmail.com>
2021-07-22 10:28:45 +0200

    build

--- build/Build.cs
+++ build/Build.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.IO;
 using System.Linq;
@@ -10,6 +11,7 @@ using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Docker;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
@@ -18,6 +20,8 @@ using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.IO.CompressionTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.Docker.DockerTasks;
+using LogLevel = Nuke.Common.LogLevel;
 
 [CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
@@ -37,14 +41,29 @@ class Build : NukeBuild
     [GitRepository]                    readonly GitRepository GitRepository;
     [GitVersion(Framework = "net5.0")] readonly GitVersion    GitVersion;
 
-    Project MainProject => Solution.GetProject("journey-markdown-converter");
+    string ProjectName => "journey-markdown-converter";
+    
+    Project MainProject => Solution.GetProject(ProjectName);
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
     AbsolutePath OutputDirectory => RootDirectory / "output";
     AbsolutePath PublishDirectory => RootDirectory / "publish";
     AbsolutePath DeliverablesDirectory => RootDirectory / "deliverables";
-    
+
+    public bool IsReleaseVersion => GitVersion.PreReleaseTag == string.Empty;
+
+    protected override void OnBuildInitialized()
+    {
+        base.OnBuildInitialized();
+        Logger.Log(LogLevel.Normal, $"Building '{ProjectName}' version {GitVersion.SemVer} ({GitVersion.InformationalVersion})");
+        Logger.Log(LogLevel.Normal, $"IsLocalBuild = {IsLocalBuild}");
+        Logger.Log(LogLevel.Normal, $"IsReleaseVersion = {IsReleaseVersion}");
+        Logger.Log(LogLevel.Normal, $"Configuration = {Configuration}");
+
+        Console.WriteLine($"::set-output name={nameof(GitVersion.SemVer)}::{GitVersion.SemVer}");
+    }
+
     Target Clean => _ =>
         _.Before(Restore)
          .DependsOn(CleanDeliverables, CleanPublished)
@@ -59,12 +78,12 @@ class Build : NukeBuild
     {
         EnsureCleanDirectory(DeliverablesDirectory);
     });
-    
+
     Target CleanPublished => _ => _.Executes(() =>
     {
         EnsureCleanDirectory(PublishDirectory);
     });
-    
+
     Target Restore => _ =>
         _.Executes(() =>
         {
@@ -90,11 +109,12 @@ class Build : NukeBuild
          .Produces(TestsDirectory + "/TestResults/*trx")
          .Executes(() =>
           {
-              DotNetTest(s => s
-                             .SetProjectFile(Solution)
-                             .SetConfiguration(Configuration)
-                             .EnableNoBuild()
-                             .SetLogger("trx")
+              DotNetTest(
+                  s => s
+                      .SetProjectFile(Solution)
+                      .SetConfiguration(Configuration)
+                      .EnableNoBuild()
+                      .SetLogger("trx")
               );
           });
 
@@ -105,11 +125,12 @@ class Build : NukeBuild
          .Produces(PublishDirectory)
          .Executes(() =>
           {
-              DotNetPublish(s => s.SetProject(MainProject)
-                                  .SetConfiguration(Configuration)
-                                  .MyGitVersionPublish(GitVersion)
-                                  .SetOutput(PublishDirectory / "portable")
-                                  .EnableNoBuild()
+              DotNetPublish(
+                  s => s.SetProject(MainProject)
+                        .SetConfiguration(Configuration)
+                        .MyGitVersionPublish(GitVersion)
+                        .SetOutput(PublishDirectory / "portable")
+                        .EnableNoBuild()
               );
           });
 
@@ -153,7 +174,7 @@ class Build : NukeBuild
                   var dirName = Path.GetFileName(dir);
                   var extension = (dirName.StartsWith("win") || dirName == "portable") ? ".zip" : ".tar.gz";
 
-                  Compress(dir, DeliverablesDirectory / $"journey-markdown-converter-{dirName}-{GitVersion.SemVer}{extension}");
+                  Compress(dir, DeliverablesDirectory / $"{ProjectName}-{dirName}-{GitVersion.SemVer}{extension}");
               }
           });
 
@@ -163,6 +184,25 @@ class Build : NukeBuild
     Target CreatePortableDeliverable => _ =>
         _.DependsOn(PublishPortable, ArchivePublished);
 
+    Target DockerBuild => _ =>
+        _.DependsOn(PublishPortable)
+         .Executes(() =>
+          {
+              var tags = from version in new[]
+                         {
+                             GitVersion.SemVer,
+                             IsReleaseVersion ? "latest" : null
+                         }
+                         where version != null
+                         select $"{ProjectName}:{version}";
+
+              DockerBuildxBuild(
+                  s => s.SetPath(RootDirectory)
+                        .SetTag(tags)
+                        .EnableLoad());
+          });
+
+
     Target QuickCi => _ =>
         _.DependsOn(Test, CreatePortableDeliverable);
 
@@ -170,15 +210,19 @@ class Build : NukeBuild
         _.DependsOn(Test, CreateDeliverables);
 }
 
-static class BuildExtensions
+static class MyGitVersionExtensions
 {
+    private static string AssemblyVersion(this GitVersion gitVersion) => gitVersion.AssemblySemVer;
+    private static string FileVersion(this GitVersion gitVersion) => gitVersion.AssemblySemFileVer;
+    private static string InformationalVersion(this GitVersion gitVersion) => gitVersion.InformationalVersion;
+
     public static T MyGitVersionBuild<T>(this T toolSettings, GitVersion gitVersion) where T : DotNetBuildSettings =>
-        toolSettings.SetAssemblyVersion(gitVersion.AssemblySemVer)
-                    .SetFileVersion(gitVersion.AssemblySemFileVer)
-                    .SetInformationalVersion(gitVersion.InformationalVersion);
+        toolSettings.SetAssemblyVersion(gitVersion.AssemblyVersion())
+                    .SetFileVersion(gitVersion.FileVersion())
+                    .SetInformationalVersion(gitVersion.InformationalVersion());
 
     public static T MyGitVersionPublish<T>(this T toolSettings, GitVersion gitVersion) where T : DotNetPublishSettings =>
-        toolSettings.SetAssemblyVersion(gitVersion.AssemblySemVer)
-                    .SetFileVersion(gitVersion.AssemblySemFileVer)
-                    .SetInformationalVersion(gitVersion.InformationalVersion);
+        toolSettings.SetAssemblyVersion(gitVersion.AssemblyVersion())
+                    .SetFileVersion(gitVersion.FileVersion())
+                    .SetInformationalVersion(gitVersion.InformationalVersion());
 }
